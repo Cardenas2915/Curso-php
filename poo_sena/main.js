cf806a73b37522514d20877eeb623256b98521eb
SOFTWARE <SOFTWARE@YOPDFRCN10F922.FormacionSena.RED>
2023-03-07 07:56:05 -0500

    js

--- poo_sena/main.js
+++ poo_sena/main.js
@@ -1,6 +1,6 @@
 function promedio() {
-
-    var cantidad = parseInt(prompt("Ingresa  cuantas notas desea ingresar"));
+    var mensaje = document.querySelector('#mensaje');
+    var cantidad = parseInt(prompt("cuantas notas desea ingresar"));
     var notas = 0;
     var promedio = 0;
     
@@ -15,16 +15,16 @@ function promedio() {
     promedio=promedio.toFixed(1);
 
     if(promedio <= 59){
-        document.write("su nota es "  + promedio +  " usted tiene un desempeño bajo");
+        mensaje.innerHTML = "<h1 class='notification is-info is-light title is-3'>su nota es " + promedio +" su promedio es bajo</h1>";
         
     } else if(promedio <= 69){
-        document.write("su nota es "  + promedio +  " usted tiene un desempeño basico");
+        mensaje.innerHTML = "<h1 class='notification is-info is-light title is-3'>su nota es " + promedio +" su promedio es basico</h1>";
 
     } else if(promedio <= 79){
-        document.write("su nota es "  + promedio +  " usted tiene un desempeño alto");
+        mensaje.innerHTML = "<h1 class='notification is-info is-light title is-3'>su nota es " + promedio +" su promedio es alto</h1>";
 
     } else if(promedio <= 100){
-        document.write("su nota es "  + promedio +  " usted tiene un desempeño superior");
+        mensaje.innerHTML = "<h1 class='notification is-info is-light title is-3'>su nota es " + promedio +" su promedio es superior</h1>";
     }
 }
 
@@ -32,3 +32,110 @@ function promedio() {
 
 
 
+function validacion(){
+
+    var mensaje = document.querySelector('#mensaje');
+    var caracter = prompt("ingrese su informacion");
+    
+while(caracter===""){
+    alert("ingrese un caracter valido");
+    caracter = prompt("ingrese su informacion");
+    
+}
+
+if(isNaN(caracter)=== true){
+    mensaje.innerHTML = "<h1 class='notification is-info is-light title is-3'>Ingresaste una cadena de texto</h1>";
+
+}else{
+    mensaje.innerHTML ="<h1 class='notification is-info is-light title is-3'>Ingresaste un numero</h1>";
+}
+    
+}
+
+
+
+function generar(){
+
+    var mensaje = document.querySelector('#mensaje');
+    var numero = Math.random()*100;
+    numero = Math.round(numero)
+    
+    mensaje.innerHTML = "<h1 class='notification is-info is-light title is-1'>" + numero + "</h1>"
+}
+
+
+
+function numeros(){
+
+
+    var ms1 = document.querySelector('#ms1');
+    var ms2 = document.querySelector('#ms2');
+    var ms3 = document.querySelector('#ms3');
+
+        var numero1 = Math.random()*100;
+        numero1 = Math.round(numero1);
+        
+        ms1.innerHTML = "<h1 class='button box title is-2'>" + numero1 + "</h1>"
+
+        var numero2 = Math.random()*100;
+        numero2 = Math.round(numero2);
+
+        if(numero1 == numero2){
+            numero1++;
+        }
+            ms2.innerHTML = "<h1 class='button box title is-2'>" + numero2 + "</h1>"
+
+        var numero3 = Math.random()*100;
+        numero3 = Math.round(numero3);
+
+        if(numero2 == numero3 || numero1 == numero3){
+            numero3++;
+        }
+            ms3.innerHTML = "<h1 class='button box title is-2'>" + numero3 + "</h1>"
+        
+        
+        
+}
+
+function mayor() {
+
+    var mensajeMayor = document.querySelector('#mensajeMayor');
+    var nombre1 = document.querySelector('#nombre1');
+    var nombre2 = document.querySelector('#nombre2');
+    var nombre3 = document.querySelector('#nombre3');
+
+    var edad1 = document.querySelector('#edad1');
+    var edad2 = document.querySelector('#edad2');
+    var edad3 = document.querySelector('#edad3');
+
+
+    var persona1 = prompt("Ingrese el nombre de la primera persona");
+    var edadP1 = prompt("Ingrese su edad");
+
+    var persona2 = prompt("Ingrese el nombre de la segunda persona");
+    var edadP2 = prompt("Ingrese su edad");
+
+    var persona3 = prompt("Ingrese el nombre de la tercera persona");
+    var edadP3 = prompt("Ingrese su edad");
+
+    nombre1.innerHTML = '<p>' + persona1 + '</p>'
+    nombre2.innerHTML = '<p>' + persona2 + '</p>'
+    nombre3.innerHTML = '<p>' + persona3 + '</p>'
+
+    edad1.innerHTML = '<p>' + edadP1 + '</p>'
+    edad2.innerHTML = '<p>' + edadP2 + '</p>'
+    edad3.innerHTML = '<p>' + edadP3 + '</p>'
+
+    if(edadP1 > edadP2 && edad1 > edadP3){
+        mensajeMayor.innerHTML = "<h1 class='button box title is-2'>" + persona1 + " es mayor</h1>"
+    }
+    
+    if(edadP2 > edadP1 && edad2 > edadP3){
+        mensajeMayor.innerHTML = "<h1 class='button box title is-2'>" + persona2 + " es mayor</h1>"
+    }
+
+    if(edadP3 > edadP1 && edadP3 > edadP2){
+        mensajeMayor.innerHTML = "<h1 class='button box title is-2'>" + persona3 + " es mayor</h1>"
+    }
+
+}
--- poo_sena/sayayn.php
+++ poo_sena/sayayn.php
@@ -2,23 +2,30 @@
 
 class Saiyajin{
 
-    public string $nombre="Goku";
-    public int $nivel_pelea=1000;
+    public string $nombre;
+    public int $nivel_pelea;
 
-    public function saludar(){
-        return "hola soy ".$this->nombre
+    public function __construct($nombre,$nivel_pelea){
+        $this->nombre=$nombre;
+        $this->nivel_pelea=$nivel_pelea;
+    }
+
+    public function saludar($texto="hola soy "){
+        return $texto.$this->nombre;
 
     }
 
     public function NivelDePelea(){
-    return $this->nombre." tiene un nivel de pelea de ".$this->nivel_pelea
+    return $this->nombre." tiene un nivel de pelea de ".$this->nivel_pelea;
     }
 }
 
-$goku = new Saiyajin();
+$goku = new Saiyajin("goku",1000);
+echo $goku->saludar();
 //var_dump($goku) esto es para ver que tipo de datos es esta variable
 
-echo $goku->nombre;
-echo $goku->NivelDePelea();
+echo '<br>';
+echo '<br>';
 
-$vegueta = new Saiyajin();
+$vegueta = new Saiyajin("vegeta",950);
+echo $vegueta->saludar("mi nombre es ");
